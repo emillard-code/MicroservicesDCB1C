df977c944895c0910bc1b7e7a5ecb77de0b05fe1
Emerson Millard <emillard.code@gmail.com>
2022-07-21 17:06:05 -0700

    Initial commit

---
+++ src/test/java/com/project/microservicesdcb1c/MicroservicesDcb1CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservicesdcb1c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDcb1CApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
